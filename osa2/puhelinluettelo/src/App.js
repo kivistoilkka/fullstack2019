72851c4628511fcfaae6d131750b1a7855dd33cb
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2019-09-25 16:14:37 +0300

    Exercise 2.15 puhelinluettelo step7

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -25,10 +25,15 @@ const App = () => {
         name: newName,
         number: newNumber
       }
-  
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+      
+      axios
+        .post('http://localhost:3001/persons', personObject)
+        .then(response => {
+          setPersons(persons.concat(personObject))
+          setNewName('')
+          setNewNumber('')
+        })
+        
     } else {
       window.alert(`${newName} is already added to phonebook`)
     }
