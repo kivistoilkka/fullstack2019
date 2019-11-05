d3657154e16147fc280b527bfcb60de0abb2e772
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2019-11-05 19:38:22 +0200

    Exercise 4.2 blogilista, step2, ready

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,19 +1,10 @@
 const config = require('./utils/config')
-const http = require('http')
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
+const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
@@ -26,22 +17,6 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
