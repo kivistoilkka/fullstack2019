76e83c7da1ceaa64e96d482ce1a8edd47cf28268
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2020-01-09 18:37:04 +0200

    Exercise 4.18 blogilistan laajennus, step7

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 mongoose.set('useFindAndModify', false)
@@ -24,6 +25,7 @@ app.use(bodyParser.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.errorHandler)
 
