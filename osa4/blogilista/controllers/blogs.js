afe1120cec99ab2ecc356be502381ab064276ce3
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2020-01-09 15:25:21 +0200

    Exercise 4.17 blogilistan laajennus, step6

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,19 +1,24 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 const logger = require('../utils/logger')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1, id: 1 })
   response.json(blogs)
 })
   
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findOne({})
+
   const blog = new Blog({
+    url: body.url,
     title: body.title,
     author: body.author,
-    url: body.url,
+    user: user._id,
     likes: body.likes === undefined ? 0 : body.likes
   })
 
@@ -21,6 +26,8 @@ blogsRouter.post('/', async (request, response) => {
     response.status(400).end()
   } else {
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
     response.status(201).json(savedBlog)
   }
 })
